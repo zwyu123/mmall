e4f2eb7d3b1438d50c698ed8c1517a546281fd1a
zwyu123 <zwyu2017@163.com>
2018-10-15 23:29:43 +0800

    获取用户详细信息功能

---
+++ src/main/java/com/mmall/controller/backend/UserManageController.java
@@ -0,0 +1,22 @@
+package com.mmall.controller.backend;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/manage/user")
+public class UserManageController {
+
+    @Autowired
+    private IUserService iUserService;
+
+    public ServerResponse<User> login(String username, String password, HttpSession session){
+        ServerResponse<User>
+    }
+}
--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -1,6 +1,7 @@
 package com.mmall.controller.portal;
 
 import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
@@ -150,5 +151,20 @@ public class UserController {
             session.setAttribute(Const.CURRENT_USER,response.getData());
         }
         return response;
+    }
+
+    /**
+     * 获取用户详细信息
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "get_information.do",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<User> get_information(HttpSession session){
+        User currentUser = (User)session.getAttribute(Const.CURRENT_USER);
+        if (currentUser == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"未登录，需要强制登录status=10");
+        }
+        return iUserService.getInformation(currentUser.getId());
     }
 }
