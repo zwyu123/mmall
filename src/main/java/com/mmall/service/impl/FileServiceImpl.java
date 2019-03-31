b5b43f775cf065c3f79298c565effb55e8f13148
zwyu123 <zwyu2017@163.com>
2019-03-31 21:56:40 +0800

    FTPUtil

--- src/main/java/com/mmall/service/impl/FileServiceImpl.java
+++ src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -1,9 +1,17 @@
 package com.mmall.service.impl;
 
 import com.mmall.service.IFileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
 public class FileServiceImpl implements IFileService {
+
+    private Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
 
     public String upload(MultipartFile file,String path){
         //拿到上传文件的原始文件名
@@ -11,5 +19,28 @@ public class FileServiceImpl implements IFileService {
         //获取扩展名
         //abc.jpg
         String fileExtensionName = fileName.substring(fileName.lastIndexOf(".")+1);
+        //防止两次上传图片的文件名相同。例如：第一次传A:abc.jpg, 第二次传B:abc.jpg, 这样第二次会覆盖第一次的图片
+        String uploadFileName = UUID.randomUUID().toString()+"."+fileExtensionName ;
+        logger.info("开始上传文件，上传文件的文件名:{},上传的路径:{},新文件名:{}",fileName,path,uploadFileName);
+
+        File fileDir = new File(path);
+        if (!fileDir.exists()){
+            fileDir.setWritable(true);
+            fileDir.mkdirs();
+        }
+        File targetFile = new File(path,uploadFileName);
+
+        try {
+            file.transferTo(targetFile);
+            //文件已经上传成功了
+
+            //todo 将targetFile上传到我们的FTP服务器上
+
+            //todo 上传完之后，删除upload下面的文件
+        } catch (IOException e) {
+            logger.error("上传文件异常",e);
+            return null;
+        }
+        return targetFile.getName();
     }
 }
