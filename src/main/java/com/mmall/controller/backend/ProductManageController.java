0b41f6f8f022eef7d6afd542c1d0951943bd1472
zwyu123 <zwyu2017@163.com>
2019-04-01 21:59:57 +0800

    upload

--- src/main/java/com/mmall/controller/backend/ProductManageController.java
+++ src/main/java/com/mmall/controller/backend/ProductManageController.java
@@ -1,12 +1,15 @@
 package com.mmall.controller.backend;
 
+import com.google.common.collect.Maps;
 import com.mmall.common.Const;
 import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.Product;
 import com.mmall.pojo.User;
+import com.mmall.service.IFileService;
 import com.mmall.service.IProductService;
 import com.mmall.service.IUserService;
+import com.mmall.util.PropertiesUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/manage/product")
@@ -25,6 +29,8 @@ public class ProductManageController {
     private IUserService iUserService;
     @Autowired
     private IProductService iProductService;
+    @Autowired
+    private IFileService iFileService;
 
     @RequestMapping("save.do")
     @ResponseBody
@@ -103,9 +109,50 @@ public class ProductManageController {
         }
     }
 
-    public ServerResponse upload(MultipartFile file, HttpServletRequest request){
-        //path 相对路径 从request的session里边拿到servlet的上下文，通过getRealPath()方法，上传的文件夹叫upload
-        //这个路径上传完之后会创建到我们发布之后的webapp/WEB-INF下，和index.jsp同级
-        String path = request.getSession().getServletContext().getRealPath("upload");
+    @RequestMapping("upload.do")
+    @ResponseBody
+    public ServerResponse upload(HttpSession session,@RequestParam(value = "upload_file",required = false) MultipartFile file, HttpServletRequest request){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录，请登录管理员");
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()){
+            //path 相对路径 从request的session里边拿到servlet的上下文，通过getRealPath()方法，上传的文件夹叫upload
+            //这个路径上传完之后会创建到我们发布之后的webapp/WEB-INF下，和index.jsp同级
+            String path = request.getSession().getServletContext().getRealPath("upload");
+            String targetFileName = iFileService.upload(file,path);
+            String url = PropertiesUtil.getProperty("ftp.server.http.prefix") + targetFileName;
+
+            Map fileMap = Maps.newHashMap();
+            fileMap.put("uri",targetFileName);
+            fileMap.put("url",url);
+
+            return ServerResponse.createBySuccess(fileMap);
+        }else {
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+    @RequestMapping("richtext_img_upload.do")
+    @ResponseBody
+    public ServerResponse richtextImgUpload(HttpSession session,@RequestParam(value = "upload_file",required = false) MultipartFile file, HttpServletRequest request){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录，请登录管理员");
+        }
+        //富文本中对于返回值有自己的要求，我们使用是simditor所以按照simditor的要求进行返回
+        if (iUserService.checkAdminRole(user).isSuccess()){
+            String path = request.getSession().getServletContext().getRealPath("upload");
+            String targetFileName = iFileService.upload(file,path);
+            String url = PropertiesUtil.getProperty("ftp.server.http.prefix") + targetFileName;
+
+            Map fileMap = Maps.newHashMap();
+            fileMap.put("uri",targetFileName);
+            fileMap.put("url",url);
+
+            return ServerResponse.createBySuccess(fileMap);
+        }else {
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
     }
 }
--- src/main/java/com/mmall/service/impl/FileServiceImpl.java
+++ src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -1,14 +1,18 @@
 package com.mmall.service.impl;
 
+import com.google.common.collect.Lists;
 import com.mmall.service.IFileService;
+import com.mmall.util.FTPUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
+@Service("iFileService")
 public class FileServiceImpl implements IFileService {
 
     private Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
@@ -23,20 +27,26 @@ public class FileServiceImpl implements IFileService {
         String uploadFileName = UUID.randomUUID().toString()+"."+fileExtensionName ;
         logger.info("开始上传文件，上传文件的文件名:{},上传的路径:{},新文件名:{}",fileName,path,uploadFileName);
 
+        //创建目录
         File fileDir = new File(path);
         if (!fileDir.exists()){
+            //赋予权限，可写
             fileDir.setWritable(true);
             fileDir.mkdirs();
         }
+        //创建文件
         File targetFile = new File(path,uploadFileName);
 
         try {
-            file.transferTo(targetFile);
+            //保存文件，把内存图片写入磁盘中
+            //没有这句话，生成的文件就是一个文件夹。有了以后，就会在path路径下，生成一个文件
             //文件已经上传成功了
-
-            //todo 将targetFile上传到我们的FTP服务器上
-
-            //todo 上传完之后，删除upload下面的文件
+            file.transferTo(targetFile);
+            //将targetFile上传到我们的FTP服务器上
+            //已经上传到ftp服务器上
+            FTPUtil.uploadFile(Lists.newArrayList(targetFile));
+            //上传完之后，删除upload下面的文件
+            targetFile.delete();
         } catch (IOException e) {
             logger.error("上传文件异常",e);
             return null;
