a93c633ad3c086dcc0510bbff506af71b9a95a7c
zwyu123 <zwyu2017@163.com>
2019-04-05 21:29:11 +0800

    Cart add

--- src/main/java/com/mmall/controller/portal/CartController.java
+++ src/main/java/com/mmall/controller/portal/CartController.java
@@ -4,6 +4,9 @@ import com.mmall.common.Const;
 import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
+import com.mmall.service.ICartService;
+import com.mmall.vo.CartVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,10 +16,14 @@ import javax.servlet.http.HttpSession;
 @RequestMapping("/cart/")
 public class CartController {
 
-    public ServerResponse add(HttpSession session,Integer count,Integer productId){
+    @Autowired
+    private ICartService iCartService;
+
+    public ServerResponse<CartVo> add(HttpSession session, Integer count, Integer productId){
         User user = (User)session.getAttribute(Const.CURRENT_USER);
         if (user == null){
             return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
         }
+        return iCartService.add(user.getId(),productId,count);
     }
 }
--- src/main/java/com/mmall/dao/CartMapper.java
+++ src/main/java/com/mmall/dao/CartMapper.java
@@ -3,6 +3,8 @@ package com.mmall.dao;
 import com.mmall.pojo.Cart;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface CartMapper {
     int deleteByPrimaryKey(Integer id);
 
@@ -17,4 +19,8 @@ public interface CartMapper {
     int updateByPrimaryKey(Cart record);
 
     Cart selectCartByUserIdProductId(@Param("userId") Integer userId, @Param("productId") Integer productId);
+
+    List<Cart> selectCartByUserId(Integer userId);
+
+    int selectCartProductCheckedStatusByUserId(Integer userId);
 }
