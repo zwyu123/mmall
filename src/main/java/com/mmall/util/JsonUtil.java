0e027b6bd93be81d305f23b8b843afd0afd173b0
zwyu123 <zwyu2017@163.com>
2019-06-20 23:32:12 +0800

    JsonUtil

--- src/main/java/com/mmall/util/JsonUtil.java
+++ src/main/java/com/mmall/util/JsonUtil.java
@@ -1,14 +1,18 @@
 package com.mmall.util;
 
-import com.sun.xml.internal.ws.developer.Serialization;
+import com.google.common.collect.Lists;
+import com.mmall.pojo.User;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.codehaus.jackson.map.DeserializationConfig;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.SerializationConfig;
 import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+import org.codehaus.jackson.type.JavaType;
+import org.codehaus.jackson.type.TypeReference;
 
-import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 @Slf4j
 public class JsonUtil {
@@ -50,12 +54,80 @@ public class JsonUtil {
         try {
             return obj instanceof String ? (String)obj : objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
         } catch (Exception e) {
-            log.warn("Parse object to String error",e);
+            log.warn("Parse Object to String error",e);
             return null;
         }
     }
 
-    public static <T> string2Obj(String str,Class<T> clazz){
-        if ()
+    public static <T> T string2Obj(String str,Class<T> clazz){
+        if (StringUtils.isEmpty(str) || clazz == null){
+            return null;
+        }
+
+        try {
+            return clazz.equals(String.class)? (T)str : objectMapper.readValue(str,clazz);
+        } catch (Exception e) {
+            log.warn("Parse String to Object error",e);
+            return null;
+        }
+    }
+
+    public static <T> T string2Obj(String str, TypeReference<T> typeReference){
+        if (StringUtils.isEmpty(str) || typeReference == null){
+            return null;
+        }
+
+        try {
+            return (T)(typeReference.getType().equals(String.class)? str : objectMapper.readValue(str,typeReference));
+        } catch (Exception e) {
+            log.warn("Parse String to Object error",e);
+            return null;
+        }
+    }
+
+    public static <T> T string2Obj(String str, Class<?> collectionClass, Class<?>... elementClasses){
+        JavaType javaType = objectMapper.getTypeFactory().constructParametricType(collectionClass,elementClasses);
+        try {
+            return objectMapper.readValue(str,javaType);
+        } catch (Exception e) {
+            log.warn("Parse String to Object error",e);
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        User u1 = new User();
+        u1.setId(1);
+        u1.setEmail("geely@happymmall.com");
+
+        User u2 = new User();
+        u2.setId(2);
+        u2.setEmail("geelyu2@happymmall.com");
+
+        String user1Json = JsonUtil.obj2String(u1);
+
+        String user1JsonPretty = JsonUtil.obj2StringPretty(u1);
+
+        log.info("user1Json:{}",user1Json);
+
+        log.info("user1JsonPretty:{}",user1JsonPretty);
+
+        User user = JsonUtil.string2Obj(user1Json,User.class);
+
+        List<User> userList = Lists.newArrayList();
+        userList.add(u1);
+        userList.add(u2);
+
+        String userListStr = JsonUtil.obj2StringPretty(userList);
+
+        log.info("================");
+
+        log.info(userListStr);
+
+        List<User> userListObj1 = JsonUtil.string2Obj(userListStr, new TypeReference<List<User>>() {
+        });
+
+        List<User> userListObj2 = JsonUtil.string2Obj(userListStr,List.class,User.class);
+        System.out.println("end");
     }
 }
